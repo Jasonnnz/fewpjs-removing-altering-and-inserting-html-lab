f117d0c7762f68a0e5e22b3c545dc3d7d5da157a
Jason Zhou <jzhou35@buffalo.edu>
2021-01-11 13:01:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+let mainNode = document.querySelector('main#main');
+mainNode.remove();
+
+// let newHeader = document.querySelector('h1#victory');
+let newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+let name = 'Jason';
+newHeader.id = 'victory';
+newHeader.innerHTML = `${name}is the champion`;
